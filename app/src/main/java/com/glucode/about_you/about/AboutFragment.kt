3f6fa52568f96878590198c7029ab784e97291dc
Tlhogi Manye <tlhogi.manye@byteorbit.com>
2024-07-23 23:01:29 +0200

    Add the gallery selection of photos to the profile view

--- app/src/main/java/com/glucode/about_you/about/AboutFragment.kt
+++ app/src/main/java/com/glucode/about_you/about/AboutFragment.kt
@@ -1,16 +1,21 @@
 package com.glucode.about_you.about
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.glucode.about_you.about.views.ProfileCardView
 import com.glucode.about_you.about.views.QuestionCardView
 import com.glucode.about_you.databinding.FragmentAboutBinding
 import com.glucode.about_you.mockdata.MockData
 
-class AboutFragment: Fragment() {
+class AboutFragment : Fragment() {
     private lateinit var binding: FragmentAboutBinding
+    private val REQUEST_IMAGE_PICK = 1001
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,6 +49,19 @@ class AboutFragment: Fragment() {
     private fun setUpProfile() {
         val engineerName = arguments?.getString("name")
         val engineer = MockData.engineers.first { it.name == engineerName }
+
         binding.profileView.setProfile(engineer)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == Activity.RESULT_OK) {
+            val imageUri = data?.data
+            if (imageUri != null) {
+                // Save image and update UI
+                binding.profileView.saveImageUri(imageUri)
+                binding.profileView.setProfileImage(imageUri)
+            }
+        }
     }
 }
--- app/src/main/java/com/glucode/about_you/about/views/ProfileCardView.kt
+++ app/src/main/java/com/glucode/about_you/about/views/ProfileCardView.kt
@@ -1,13 +1,17 @@
 package com.glucode.about_you.about.views
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.glucode.about_you.R
 import com.glucode.about_you.databinding.ViewProfileCardBinding
+import com.glucode.about_you.mockdata.MockData
 import com.glucode.about_you.engineers.models.Engineer
 
 class ProfileCardView @JvmOverloads constructor(
@@ -16,6 +20,7 @@ class ProfileCardView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : CardView(context, attrs, defStyleAttr) {
 
+    private val REQUEST_IMAGE_PICK = 1001
     private val binding: ViewProfileCardBinding = ViewProfileCardBinding.inflate(
         LayoutInflater.from(context), this, true
     )
@@ -24,9 +29,13 @@ class ProfileCardView @JvmOverloads constructor(
         radius = resources.getDimension(R.dimen.corner_radius_normal)
         elevation = resources.getDimension(R.dimen.elevation_normal)
         setCardBackgroundColor(ContextCompat.getColor(context, R.color.black))
-        setOnClickListener {
-            // Open gallery to select a new profile image
-            // (You need to implement this part with an Intent and handle result)
+
+        // Set an OnClickListener to pick an image from the gallery
+        binding.profileImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            if (context is Activity) {
+                context.startActivityForResult(intent, REQUEST_IMAGE_PICK)
+            }
         }
     }
 
@@ -36,10 +45,34 @@ class ProfileCardView @JvmOverloads constructor(
         binding.quickStatsYears.text = engineer.quickStats.years.toString()
         binding.quickStatsCoffees.text = engineer.quickStats.coffees.toString()
         binding.quickStatsBugs.text = engineer.quickStats.bugs.toString()
-        // Load the profile image using Glide or any other image loading library
-       /* Glide.with(context)
-            .load(engineer.defaultImageName)
-            .placeholder(R.drawable.ic_person) // Add a placeholder image if needed
-            .into(binding.profileImage)*/
+
+        // Load the profile image
+        val sharedPreferences = context.getSharedPreferences("ProfilePrefs", Context.MODE_PRIVATE)
+        val imageUriString = sharedPreferences.getString("profileImageUri", null)
+        if (imageUriString != null) {
+            val imageUri = Uri.parse(imageUriString)
+            binding.profileImage.setImageURI(imageUri)
+        } else {
+            // Load default image if no URI is saved
+            binding.profileImage.setImageResource(R.drawable.ic_person)
+        }
+    }
+
+    fun setProfileImage(imageUri: Uri) {
+        binding.profileImage.setImageURI(imageUri)
+    }
+
+    fun saveImageUri(uri: Uri) {
+        val sharedPreferences = context.getSharedPreferences("ProfilePrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("profileImageUri", uri.toString())
+        editor.apply()
+
+        // Update MockData
+        val engineerName = (context as Activity).intent.getStringExtra("name")
+        val engineer = MockData.engineers.firstOrNull { it.name == engineerName }
+        engineer?.let {
+            it.defaultImageName = uri.toString()
+        }
     }
 }
